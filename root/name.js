b06a99bed278c5b3dbb79062c1620a8b24d577c0
Jon Schlinkert <dev@sellside.com>
2013-07-06 08:28:34 -0400

    fix template error

--- root/name.js
+++ root/name.js
@@ -2,7 +2,7 @@
   module.exports.register = function(Handlebars, options) {
 
     // Customize this helper
-    Handlebars.registerHelper('{%= helper_name %}', function(str) {
+    Handlebars.registerHelper('{%= name %}', function(str) {
       var content = '<strong>' + str + '</strong>';
       return new Handlebars.SafeString(content);
     });
